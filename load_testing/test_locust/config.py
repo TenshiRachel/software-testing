603aa7ccf909886a091e8607630fcbd612b29a43
Anthony <joshdebean@gmail.com>
2020-08-09 00:52:51 +0800

    add locustfile

---
+++ load_testing/test_locust/config.py
@@ -0,0 +1,17 @@
+GLOBAL_COUNTER = 0
+
+USER_CREDENTIALS = [
+    ('110385fcd064b3050e7e9ce31f63dc510af24e2c', '')
+]
+
+PROJECT = 'software-test'
+
+URL_GET_PROJECT_BADGES = 'api/project_badges/measure?project={project}&metric=coverage'
+URL_GET_COVERAGE = 'api/measures/search_history?component={project}&metrics=coverage'
+
+def get_next(items=USER_CREDENTIALS):
+    global GLOBAL_COUNTER
+    size = len(items)
+    res = items[GLOBAL_COUNTER % size]
+    GLOBAL_COUNTER += 1
+    return res
