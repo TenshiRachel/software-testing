6157fe3392519de1adf1c0518fd23a4b27f9280e
Anthony <joshdebean@gmail.com>
2020-08-27 00:47:33 +0800

    add request functions

--- integration_testing/test_super_agent/sonar_client/test_client/sonar_client.js
+++ integration_testing/test_super_agent/sonar_client/test_client/sonar_client.js
@@ -4,7 +4,29 @@ const constants = require('../constants')
 class SonarClient {
     constructor(baseUrl=constants.baseUrl, auth=constants.auth) {
         this.baseUrl = baseUrl
-        this.auth = auth
+        this.auth = {
+            Authorization: 'Bearer ' + auth
+        }
+    }
+
+    async getProjectBranches(project) {
+        const url = this.baseUrl + constants.PROJECT_BRANCHES_ENDPOINT
+        return await superAgent.get(url)
+        .set(this.auth)
+        .query({project: project})
+    }
+
+    async getBranchCoverage(project, branch, analysisDate) {
+        const url = this.baseUrl + constants.BRANCH_COVERAGE_ENDPOINT
+        return await superAgent.get(url)
+        .set(this.auth)
+        .query({
+            component: project,
+            branch: branch,
+            from: analysisDate,
+            to: analysisDate,
+            metrics: 'coverage'
+        })
     }
 }
 
