cd9bb52eff6f969f2ef346d0c2c0551211bc8879
Anthony <joshdebean@gmail.com>
2020-08-26 23:37:28 +0800

    add request functions

--- integration_testing/test_node_got/sonar_client/test_client/sonar_client.js
+++ integration_testing/test_node_got/sonar_client/test_client/sonar_client.js
@@ -7,6 +7,28 @@ class SonarClient {
         this.auth = auth
         this.session = session(this.baseUrl, this.auth)
     }
+
+    async getProjectBranches(project) {
+        const url = constants.PROJECT_BRANCHES_ENDPOINT
+        return await this.session.get(url, {
+            searchParams: {
+                project: project
+            }
+        })
+    }
+
+    async getBranchCoverage(project, branch, analysisDate) {
+        const url = constants.BRANCH_COVERAGE_ENDPOINT
+        return await this.session.get(url, {
+            searchParams: {
+                component: project,
+                branch: branch,
+                from: analysisDate,
+                to: analysisDate,
+                metrics: 'coverage'
+            }
+        })
+    }
 }
 
 module.exports = SonarClient
