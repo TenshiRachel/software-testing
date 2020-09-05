90eca3e122d44e7c62e7999d303210214d87e181
Anthony <joshdebean@gmail.com>
2020-09-05 14:09:07 +0800

    add base elemnt

---
+++ UI_testing/test_node_selenium/page_objects/base_element.js
@@ -0,0 +1,31 @@
+class BaseElement {
+    constructor(driver, _css, _xpath='') {
+        this.driver = driver
+        this._css = _css
+        this._xpath = _xpath
+    }
+
+    async click() {
+        this.findElement().click()
+    }
+
+    async goto(url) {
+        await this.driver.get(url)
+    }
+
+    async screenshot(filename) {
+        await this.findElement()
+        await this.findElement().takeScreenshot()
+    }
+
+    async findElement() {
+        if (this._xpath == '') {
+            return await this.driver.findElement(By.css(this._css))
+        }
+        else {
+            return await this.driver.findElement(By.xpath(this._xpath))
+        }
+    }
+}
+
+module.exports = BaseElement
---
+++ UI_testing/test_node_selenium/test_sonar.js
@@ -0,0 +1,1 @@
+const webDriver = require('selenium-webdriver')
