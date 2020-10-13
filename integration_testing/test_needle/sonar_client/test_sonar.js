cda25654d78f5c53d9f09a5c983cc1d6bf6376fb
TenshiRachel <40650808+TenshiRachel@users.noreply.github.com>
2020-10-13 14:37:41 +0800

    Fix callback assert (#14)
    * fix asserion

--- integration_testing/test_needle/sonar_client/test_sonar.js
+++ integration_testing/test_needle/sonar_client/test_sonar.js
@@ -2,12 +2,11 @@ const SonarClient = require('./test_client/sonar_client')
 const constants = require('./constants')
 
 describe('sonarqube test', () => {
-    it('get branch coverage', () => {
+    it('get branch coverage', (done) => {
         const client = new SonarClient()
         client.getProjectBranches('software-test', (resStatus, resBody) => {
             expect(resStatus).toEqual(200)
             expect(resBody).toBeDefined()
-            console.log(resBody)
 
             const masterBranch = resBody.branches[0]
             const branchName = masterBranch.name
@@ -16,6 +15,7 @@ describe('sonarqube test', () => {
             client.getBranchCoverage('software-test', branchName, analysisDate, (branchResStatus, branchResBody) => {
                 expect(branchResStatus).toBe(200)
                 expect(branchResBody).toBeDefined()
+                done()
             })
         })
 
